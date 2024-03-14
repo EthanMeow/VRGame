65a811e205eed4bd1144ca5dd35b3bb74c92c4e4
EthanMeow <548887@chrome.escg.ac.uk>
2024-03-14 16:28:49 +0000

    v6

---
+++ Assets/Scripts/LoadScene1.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader1 : MonoBehaviour
+{
+    public void LoadScene()
+    {
+        SceneManager.LoadScene("Air Signs");
+    }
+}
